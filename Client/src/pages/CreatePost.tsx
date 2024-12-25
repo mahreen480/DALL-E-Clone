090af600a6bdf6be752da3271fa1f13ca57c34d0
mahreen480 <152952923+mahreen480@users.noreply.github.com>
2024-12-25 17:16:42 +0500

    frontened complete

--- Client/src/pages/CreatePost.tsx
+++ Client/src/pages/CreatePost.tsx
@@ -34,16 +34,17 @@ const CreatePost = (props: Props) => {
 
   }
 
-  const handleChange = (e) => {
-
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setForm({...form,[e.target.name]:e.target.value})
   }
 
   const handleSurpriseMe = () => {
-
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({...form, prompt: randomPrompt})
   }
 
   return (
-    <section className='max-w-7xl mx-auto'>
+    <section className='min-h-screen justify-center flex flex-col items-center max-w-7xl mx-auto'>
       <div>
         <h1 className='font-extrabold text-[32px]'>
           Create
@@ -53,7 +54,7 @@ const CreatePost = (props: Props) => {
         </p>
       </div>
 
-      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+      <form className='mt-16 max-w-3xl w-full' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-5'>
           <FormField
             labelName="Your Name"
@@ -103,6 +104,16 @@ const CreatePost = (props: Props) => {
             className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
               {generateImg ? 'Generating...' : 'Generate '}
             </button>
+        </div>
+        <div className='mt-10'>
+              <p className='mt-2 text-[#666e75] text-[14px]'>
+                Once you have created the image you want, you can share it with others with the community
+              </p>
+
+              <button type='submit' className='mt-3 text-white bg-[#6469ff] font-medium 
+              rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
+              {loading ? "Sharing.... ": "Share with the community"}
+              </button>
         </div>
       </form>
     </section>
